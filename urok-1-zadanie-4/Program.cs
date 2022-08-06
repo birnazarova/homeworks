3ded413202b4bb2079e1b52a23e55f5137c33210
birnazarova <birnazarova@gmail.com>
2022-08-05 19:20:57 -0700

    Fourth task

---
+++ urok-1-zadanie-4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить является ли число чётным
+
+Console.WriteLine("Введите любое целое число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number%2==0)
+{
+    Console.WriteLine("Число - четное.");
+}
+else
+{
+    Console.WriteLine("Число - нечетное.");
+}
