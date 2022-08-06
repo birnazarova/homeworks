e71d7ec7288c72f66dfe5399fc5b9c887c384aa1
birnazarova <birnazarova@gmail.com>
2022-08-05 19:54:34 -0700

    Sixth task

---
+++ urok-1-zadanie-6/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+Console.WriteLine("Введите трехзначное число: ");
+string number = Console.ReadLine();
+
+char[] digits = number.ToCharArray();
+ 
+Console.WriteLine(digits[2]);  
