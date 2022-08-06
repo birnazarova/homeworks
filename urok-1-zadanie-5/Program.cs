a4e530888433f14f0ffb6b2464daf4e8350cd4e5
birnazarova <birnazarova@gmail.com>
2022-08-05 19:35:06 -0700

    Fifth task

---
+++ urok-1-zadanie-5/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать числа от -N до N
+
+Console.WriteLine("Введите любое целое число: ");
+int N = int.Parse(Console.ReadLine());
+
+int[] array1 = Enumerable.Range(-N, N).ToArray();
+int[] array2 = Enumerable.Range(0, N+1).ToArray();
+Console.WriteLine(string.Join(",", array1)+","+string.Join(",", array2));
