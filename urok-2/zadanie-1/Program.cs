052ac67d160ce1bdbeea79e641ac368f42d6a1cc
birnazarova <birnazarova@gmail.com>
2022-08-09 20:38:14 -0700

    Urok 2

---
+++ urok-2/zadanie-1/Program.cs
@@ -0,0 +1,7 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+int number = new Random().Next(1,8);
+Console.WriteLine(number);
+
+if (number==1 || number==2 || number==3 || number==4 || number==5) Console.WriteLine("Это не выходной");
+else Console.WriteLine("Это выходной");
---
+++ urok-2/zadanie-2/Program.cs
@@ -0,0 +1,10 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+
+Console.WriteLine("Задайте первое число: ");
+int first_number = int.Parse(Console.ReadLine());
+Console.WriteLine("Задайте второе число: ");
+int second_number = int.Parse(Console.ReadLine());
+
+if (first_number==second_number*second_number) Console.WriteLine("Первое число является квадратом второго числа");
+else if (second_number==first_number*first_number) Console.WriteLine("Второе число является квадратом первого числа");
+else Console.WriteLine("Одно число не является квадратом другого");
