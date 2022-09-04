afd5bd781f4bccd992e389ad21255b22190f18d0
birnazarova <birnazarova@gmail.com>
2022-09-04 01:56:51 -0700

    task 5

--- urok-2/zadanie-5/Program.cs
+++ urok-2/zadanie-5/Program.cs
@@ -1,3 +1,12 @@
 ﻿// Найти кубы чисел от 1 до N
 
-Console.WriteLine("Hello, World!");
+Console.Write("Задайте число N: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+
+int[] array = new int[N];
+int pow = 3;
+
+for (int i=0; i < N; i++) //1 2 3
+{
+    Console.WriteLine($"Куб числа {i} = {Math.Pow(i, pow)}");
+}
