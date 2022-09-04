bd76896ae753527d22f1162d85582112e5d408dd
birnazarova <birnazarova@gmail.com>
2022-09-04 02:10:46 -0700

    task 4

--- urok-2/zadanie-4/Program.cs
+++ urok-2/zadanie-4/Program.cs
@@ -1,3 +1,39 @@
-﻿// Найти расстояние между точками в пространстве 2D/3D
+﻿// Найти расстояние между точками в пространстве 2D
 
-Console.WriteLine("Hello, World!");
+// Console.Write("Введите координаты x1: ");
+// int x1 = int.Parse(Console.ReadLine() ?? "0");
+
+// Console.Write("Введите координаты x2: ");
+// int x2 = int.Parse(Console.ReadLine() ?? "0");
+
+// Console.Write("Введите координаты y1: ");
+// int y1 = int.Parse(Console.ReadLine() ?? "0");
+
+// Console.Write("Введите координаты y2: ");
+// int y2 = int.Parse(Console.ReadLine() ?? "0");
+
+// double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+// Console.WriteLine($"Расстояние между двумя точками на плоскости равно {distance}");
+
+// 3D
+Console.Write("Введите координаты x1: ");
+int x1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите координаты x2: ");
+int x2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите координаты y1: ");
+int y1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите координаты y2: ");
+int y2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите координаты z1: ");
+int z1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите координаты z2: ");
+int z2 = int.Parse(Console.ReadLine());
+
+double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+Console.WriteLine($"Расстояние между двумя точками в пространстве равно {distance}");
+
